a7157815bb82c156c8e6f6fedac2a2ca5c0f9dc4
maxstar <maksimgavura78@gmail.com>
2024-12-06 18:19:02 +0200

    Добавьте файлы проекта.

---
+++ Lesson#4Task1/Lesson_4Task1.py
@@ -0,0 +1,8 @@
+﻿a1 = int(input("Введите сторону а1: "))
+a2 = int(input("Введите сторону a2: "))
+b1 = int(input("Введите сторону b1: "))
+b2 = int(input("Введите сторону b2: "))
+P = int(a1 + a2 + b1 + b2)
+S = int(a1 * b1)
+print(P, S)
+
